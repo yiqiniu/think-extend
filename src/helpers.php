145a7ba845c50ffbfd24e4563e809d639323e020
gjianbo <gjianbo@qq.com>
2019-07-15 19:42:27 +0800

    1.扩展了数组类 2.扩展了函数

--- src/helpers.php
+++ src/helpers.php
@@ -25,61 +25,185 @@ if (!function_exists('exception_api')) {
 
 
 
-/**
- * 检测浏览器类型，成功返回
- * @return string
- *
- */
-function get_browser_type(){
+if (!function_exists('result')) {
+    /** 输出返回结果
+     * @param $code
+     * @param string $msg
+     * @param array $data
+     * @throws HttpResponseException
+     */
+    function result($code, $msg = '', $data = [])
+    {
 
-    if(empty($_SERVER['HTTP_USER_AGENT'])){
-        return $_SERVER['HTTP_USER_AGENT'];
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 9.0')){
-        //return 'Internet Explorer 9.0';
-        return 'internet explorer';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 8.0')){
-        //return 'Internet Explorer 8.0';
-        return 'internet explorer';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 7.0')){
-        //return 'Internet Explorer 7.0';
-        return 'internet explorer';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 6.0')){
-        //return 'Internet Explorer 6.0';
-        return 'internet explorer';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Edge')){
-        //return 'Firefox';
-        return 'edge';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Firefox')){
-        //return 'Firefox';
-        return 'firefox';
-    }
-
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Chrome')){
-        //return 'Chrome';
-        $str = substr($_SERVER['HTTP_USER_AGENT'],strpos($_SERVER['HTTP_USER_AGENT'],'Chrome')+7,2);
-        return $str>'45'?'chrome':'chrome45';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Safari')){
-        // return 'Safari';
-        return 'safari';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Opera')){
-        // return 'Opera';
-        return 'opera';
-    }
-    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'360SE')){
-        //   return '360SE';
-        return '360se';
+        if ($code instanceof yiqiniu\exception\ApiException) {
+            $data = $code->getData();
+            if (isset($data['data']))
+                $data = $data['data'];
+            $msg = $code->getMessage();
+            $code = $code->getCode();
+        } elseif ($code instanceof think\Exception) {
+            // 记录异常
+            Logger::exception($code);
+            $msg = $code->getMessage();
+            $code = $code->getCode();
+        } elseif (is_object($code)) {
+            $data = $code->toArray();
+            $code = 200;
+        } else if (is_array($code)) {
+            $data = $code;
+            $code = 200;
+        } else if (is_string($code)) {
+            $msg = $code;
+            $code = 200;
+        } else {
+            $code = 200;
+        }
+        $result = [
+            'code' => $code,
+            'msg' => $msg != '' ? $msg : (config('status.')[$code]),
+            'time' => time(),
+            'data' => $data
+        ];
+        $response = Response::create($result, 'json');
+        throw new HttpResponseException($response);
     }
 }
 
 
+
+if (!function_exists('result')) {
+    /**
+     * 发送HTTP请求方法，目前只支持CURL发送请求
+     * @param string $url 请求URL
+     * @param array $params 请求参数
+     * @param string $method 请求方法GET/POST
+     * @param bool $upload
+     * @param array $header
+     * @return array  $data   响应数据
+     * @throws \Exception
+     */
+    function httpRequest($url, $params = [], $method = 'GET', $upload = false, $header = [])
+    {
+        try {
+            $method = strtoupper($method);
+            $opts = [
+                CURLOPT_TIMEOUT => 60,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_HTTPHEADER => $header,
+                CURLOPT_AUTOREFERER => true,
+                CURLOPT_FOLLOWLOCATION => 1
+            ];
+            /* 根据请求类型设置特定参数 */
+            switch (strtoupper($method)) {
+                case 'GET':
+                    if (!empty($params)) {
+                        $params = is_array($params) ? http_build_query($params) : $params;
+                        $url = $url . (strpos($url, "?") > 0 ? "&" : "?") . $params;
+                    }
+                    $opts[CURLOPT_URL] = $url;
+                    break;
+                case 'POST':
+                    $opts[CURLOPT_URL] = $url;
+                    $opts[CURLOPT_POST] = 1;
+                    //判断是否传输文件
+                    if ($upload) {        //设置上传文件
+                        $file = new \CURLFile($upload['file'], $upload['type'], $upload['name']);
+                        $params[] = $file;
+                    }
+                    $opts[CURLOPT_POSTFIELDS] = $params;
+                    break;
+                default:
+                    exception_api(400, '不支持的请求方式！');
+            }
+            /* 初始化并执行curl请求 */
+            $ch = curl_init();
+            curl_setopt_array($ch, $opts);
+            $data = curl_exec($ch);
+            $error = curl_error($ch);
+            curl_close($ch);
+            if ($error)
+                exception_api(400, '请求发生错误：' . $error);
+            return $data;
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}
+
+if (!function_exists('writelog')) {
+    /**
+     * 写入日志
+     * @param  $content  string  要写入的日志
+     * @append  boole  追加 true   false 不追加
+     *
+     *
+     */
+    function writelog($content, $append = true)
+    {
+        Logger::log($content, $append);
+    }
+
+}
+
+
+if (!function_exists('get_browser_type')) {
+    /**
+     * 检测浏览器类型，成功返回
+     * @return string
+     *
+     */
+    function get_browser_type()
+    {
+
+        if (empty($_SERVER['HTTP_USER_AGENT'])) {
+            return $_SERVER['HTTP_USER_AGENT'];
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 9.0')) {
+            //return 'Internet Explorer 9.0';
+            return 'internet explorer';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 8.0')) {
+            //return 'Internet Explorer 8.0';
+            return 'internet explorer';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7.0')) {
+            //return 'Internet Explorer 7.0';
+            return 'internet explorer';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.0')) {
+            //return 'Internet Explorer 6.0';
+            return 'internet explorer';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Edge')) {
+            //return 'Firefox';
+            return 'edge';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+            //return 'Firefox';
+            return 'firefox';
+        }
+
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
+            //return 'Chrome';
+            $str = substr($_SERVER['HTTP_USER_AGENT'], strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') + 7, 2);
+            return $str > '45' ? 'chrome' : 'chrome45';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Safari')) {
+            // return 'Safari';
+            return 'safari';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Opera')) {
+            // return 'Opera';
+            return 'opera';
+        }
+        if (false !== strpos($_SERVER['HTTP_USER_AGENT'], '360SE')) {
+            //   return '360SE';
+            return '360se';
+        }
+    }
+}
 
 
 
--- src/library/Arrays.php
+++ src/library/Arrays.php
@@ -486,4 +486,153 @@ class Arrays
         return $arr;
     }
 
+
+    /**
+     * 返回数组中指定多列
+     * @param Array $input 需要取出数组列的多维数组
+     * @param String $column_keys 要取出的列名，逗号分隔，如不传则返回所有列
+     * @return Array
+     */
+   public function array_columns($input, $column_keys = null, $pk = null)
+    {
+        $result = array();
+        $keys = empty($column_keys) || $column_keys == '*' ? [] : (is_array($column_keys) ? $column_keys : explode(',', $column_keys));
+        if (count($keys) == 1) {
+            $result = array_column($input, $keys[0], $pk);
+        } else {
+            if ($input) {
+                foreach ($input as $k => $v) {
+                    // 指定返回列
+                    if ($keys && $v) {
+                        $tmp = array();
+                        foreach ($keys as $key) {
+                            if ($sub_in = strpos($key, '|')) {
+                                $tmp[substr($key, $sub_in + 1)] = isset($v[substr($key, 0, $sub_in)]) ? $v[substr($key, 0, $sub_in)] : '';
+                            } else {
+                                $tmp[$key] = isset($v[$key]) ? $v[$key] : '';
+                            }
+                        }
+                    } else {
+                        $tmp = $v;
+                    }
+                    $result[$pk ? $v[$pk] : $k] = $tmp;
+                }
+            }
+        }
+        return $result;
+    }
+
+   public function info_columns($info, $column_keys = null)
+    {
+        $keys = empty($column_keys) || $column_keys == '*' ? [] : (is_array($column_keys) ? $column_keys : explode(',', $column_keys));
+        $result = false;
+        if ($info) {
+            if (count($keys) == 1) {
+                $result = $info[$keys[0]];
+            } else {
+                $tmp = false;
+                foreach ($keys as $key) {
+                    if ($sub_in = strpos($key, '|')) {
+                        $tmp[substr($key, $sub_in + 1)] = isset($info[substr($key, 0, $sub_in)]) ? $info[substr($key, 0, $sub_in)] : '';
+                    } else {
+                        $tmp[$key] = isset($info[$key]) ? $info[$key] : '';
+                    }
+                }
+                $result = $tmp;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * 把返回的数据集转换成Tree
+     * @param array $list 要转换的数据集
+     * @param string $pid parent标记字段
+     * @param string $level level标记字段
+     * @return array
+     */
+    public function list_to_tree($list, $pk = 'id', $pid = 'pid', $child = 'children', $root = 0)
+    {
+        // 创建Tree
+        $tree = array();
+        if (is_array($list)) {
+            // 创建基于主键的数组引用
+            $refer = array();
+            foreach ($list as $key => $data) {
+                $refer[$data[$pk]] =& $list[$key];
+            }
+            foreach ($list as $key => $data) {
+                // 判断是否存在parent
+                $parentId = $data[$pid];
+                if ($root == $parentId) {
+                    $tree[] =& $list[$key];
+                } else {
+                    if (isset($refer[$parentId])) {
+                        $parent =& $refer[$parentId];
+                        $parent[$child][] =& $list[$key];
+                    }
+                }
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 将list_to_tree的树还原成列表
+     * @param array $tree 原来的树
+     * @param string $child 孩子节点的键
+     * @param string $order 排序显示的键，一般是主键 升序排列
+     * @param array $list 过渡用的中间数组，
+     * @return array        返回排过序的列表数组
+     * @author yangweijie <yangweijiester@gmail.com>
+     */
+    public  function tree_to_list($tree, $child = 'children', $order = 'id', &$list = array())
+    {
+        if (is_array($tree)) {
+            foreach ($tree as $key => $value) {
+                $reffer = $value;
+                if (isset($reffer[$child])) {
+                    unset($reffer[$child]);
+                    tree_to_list($value[$child], $child, $order, $list);
+                }
+                $list[] = $reffer;
+            }
+            // $list = list_sort_by($list, $order, $sortby='asc');
+        }
+        return $list;
+    }
+
+    /**
+     * 对二维数组进行排序
+     * @param $list array       要排序的数组
+     * @param $field string     排序的字段
+     * @param string $sortby 升序或降序
+     * @return array|bool
+     */
+    public function list_sort_by($list, $field, $sortby = 'asc')
+    {
+        if (is_array($list)) {
+            $refer = $resultSet = array();
+            foreach ($list as $i => $data) {
+                $refer[$i] = &$data[$field];
+            }
+            switch ($sortby) {
+                case 'asc': // 正向排序
+                    asort($refer);
+                    break;
+                case 'desc': // 逆向排序
+                    arsort($refer);
+                    break;
+                case 'nat': // 自然排序
+                    natcasesort($refer);
+                    break;
+            }
+            foreach ($refer as $key => $val) {
+                $resultSet[] = &$list[$key];
+            }
+            return $resultSet;
+        }
+        return false;
+    }
+
 }
