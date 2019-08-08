afbe2246cc7aed269467e812e5372f639835d39c
gjianbo <gjianbo@qq.com>
2019-08-08 18:42:50 +0800

    加入对文件夹的生成

--- src/console/command/MakeFacade.php
+++ src/console/command/MakeFacade.php
@@ -10,6 +10,7 @@ use think\console\command\Make;
 use think\console\Input;
 use think\console\input\Argument;
 use think\console\Output;
+use think\Exception;
 
 /**
  * Class ModelAll
@@ -22,20 +23,29 @@ class MakeFacade extends Make
 
 
     protected $app = null;
+
     // 不能当做类名的表名
 
     protected $stubs = [
         'facade' => 'facade',
     ];
 
-    // 是否全部字段 , false 为不为空的字段,true 全部字段
-    protected $allfield = false;
+    // 忽略文件
+    protected $ignore_files = [
+        'BaseModel', 'PageBase'
+    ];
+    //
+    protected $namespace = 'app';
+
+    // 生成错误的类名
+    protected $error_files = [];
 
     protected function configure()
     {
         parent::configure();
         $this->setName('make:facade')
             ->addArgument('module', Argument::OPTIONAL, "specified Module name")
+            ->addArgument('dir', Argument::OPTIONAL, "specified app/module/dir name")
             ->setDescription('Create a new Facade class ');
     }
 
@@ -43,24 +53,97 @@ class MakeFacade extends Make
     protected function execute(Input $input, Output $output)
     {
 
-        $class_name = trim($input->getArgument('name'));
+        $arguments = $input->getArguments();
 
-        // 类不存在时返回
-        if (!class_exists($class_name)) {
-            $this->output->writeln('<error>' . $class_name . ': class not exists.</error>');
-            exit;
+        $class_name = trim($input->getArgument('name'));
+        if (is_null($arguments['module'])) {
+            $isdir = false;
+        } else {
+            $isdir = ($arguments['module'] == 'dir' && is_null($arguments['dir'])) ? true : false;
         }
 
-        $module_name = trim($input->getArgument('module'));
+
         $this->app = App::getInstance();
+
+        // 应用路径
+        $apppath = $this->app->getAppPath();
+        // 类不存在时返回
+        if (empty($class_name)) {
+            $this->output->writeln('<error>' . $class_name . ': classname not empty.</error>');
+            exit;
+        }
+        // 生成类的列表
+        $class_list = [];
+        //
+        if (!$isdir) {
+            if (!class_exists($class_name)) {
+                $this->output->writeln('<error>' . $class_name . ': class not exist.</error>');
+                exit;
+            }
+            $class_list[] = $class_name;
+            $module_name = trim($input->getArgument('module'));
+
+        } else {
+            $class_name = str_replace('/', '\\', $class_name);
+
+            $dirpath = $apppath . $class_name;
+            if (!file_exists($dirpath)) {
+                $this->output->writeln('<error>' . $class_name . ': dir path not exist.</error>');
+                exit;
+            }
+            // 生成类文件列表
+            $files = scandir($dirpath);
+            foreach ($files as $file) {
+                if ('.' . pathinfo($file, PATHINFO_EXTENSION) === '.php') {
+                    $filename = substr($file, 0, -4);
+                    if (in_array($filename, $this->ignore_files)) {
+                        continue;
+                    }
+                    $class_list[] = $this->namespace . '\\' . $class_name . '\\' . $filename;
+                }
+
+            }
+            // 没有找到文件
+            if (empty($class_list)) {
+                $this->output->writeln('<error>' . $class_name . ': dir path no found files.</error>');
+                exit;
+            }
+            $module_name = dirname($class_name);
+        }
+
+        // 获取stub代码
+        $stubs = $this->getStub();
+        $facade_stub = file_get_contents($stubs['facade']);
+
+        foreach ($class_list as $v) {
+            $this->makeFacade($v, $module_name, $facade_stub, $apppath);
+        }
+
+
+        $output->writeln('<info>' . $this->type . ':' . 'Facede Class created successfully.</info>');
+
+    }
+
+    /**
+     * 生成代理类
+     * @param $class                    类名
+     * @param $model                    模块名
+     * @param string $apppath 应用程序路径
+     * @throws \ReflectionException
+     */
+    protected function makeFacade($class_name, $module_name, $facade_stub, $apppath = '')
+    {
 
         try {
             // 解析当前类
             $ref = new ReflectionClass($class_name);
             $methods = $ref->getMethods();
+
             $funs = [];
             //解决类的所有public方法
             foreach ($methods as $method) {
+                if ($method->class !== $class_name)
+                    continue;
                 // 排除特殊的方法
                 if (substr($method->name, 0, 2) == '__')
                     continue;
@@ -74,7 +157,7 @@ class MakeFacade extends Make
                         $doc = $method->getDocComment();
                     }
 
-                    $funs[$method->name]['comment'] = str_replace([' * ',"\r","\n","\r\n"], '', $doc);
+                    $funs[$method->name]['comment'] = str_replace([' * ', "\r", "\n", "\r\n"], '', $doc);
                     //函数名称
                     $funs[$method->name]['name'] = $method->getName();
                     // 返回值
@@ -100,9 +183,9 @@ class MakeFacade extends Make
                         if (empty($param_default) && $usedefault == false) {
                             $parameter_str .= $param_type . ' $' . $param_name . ',';
                         } else {
-                            if (empty($param_default)){
-                                $param_default=" ''";
-                            }else{
+                            if (empty($param_default)) {
+                                $param_default = " ''";
+                            } else {
                                 $param_default = (empty($param_type) || $param_type == 'string') ? "'$param_default'" : $param_default;
                             }
 
@@ -127,40 +210,35 @@ class MakeFacade extends Make
             // 获取基本的类名
             $base_class_name = $this->classBaseName($class_name);
             // 判断目录是否存在
-            $apppath = $this->app->getAppPath();
+            $apppath = $apppath ?? $this->app->getAppPath();
             if (!empty($module_name)) {
                 $dirname = $apppath . $module_name . '\\facade\\';
             } else {
                 $dirname = $apppath . 'facade\\';
             }
             if (!file_exists($dirname)) {
-                mkdir($dirname, 0644, true);
+                if (!mkdir($dirname, 0644, true) && !is_dir($dirname)) {
+                    throw new \RuntimeException(sprintf('Directory "%s" was not created', $dirname));
+                }
             }
-            // 获取stub代码
-            $stubs = $this->getStub();
 
-            $facade_stub = file_get_contents($stubs['facade']);
             // 写入文件
             $model_file = $dirname . $base_class_name . '.php';
-            if (!file_exists($model_file)) {
-                file_put_contents($model_file, str_replace(['{%namespace%}', '{%className%}', ' {%methods%}', '{%fullclassname}'], [
-                    $namespace,
-                    $base_class_name,
-                    $method_str,
-                    $class_name
-                ], $facade_stub));
-            }
+            //  直接替换
+            file_put_contents($model_file, str_replace(['{%namespace%}', '{%className%}', ' {%methods%}', '{%fullclassname}'], [
+                $namespace,
+                $base_class_name,
+                $method_str,
+                $class_name
+            ], $facade_stub));
 
-            $output->writeln('<info>' . $this->type . ':' . 'Facede Class created successfully.</info>');
 
         } catch (\ReflectionException $e) {
-            throw  $e;
-
+            $error_files[$class_name] = $e->getMessage();
+        } catch (Exception $e) {
+            $error_files[$class_name] = $e->getMessage();
         }
-
-
     }
-
 
     protected function getNamespace2($model)
     {
