401a3891f6fff55a6b5be6c0f1e96a376d0408a0
gjianbo <gjianbo@qq.com>
2019-08-19 21:46:38 +0800

    改成了token有效期的了配置

--- src/const.php
+++ src/const.php
@@ -1,14 +1,15 @@
 <?php
 // 处理成功
 const API_SUCCESS = 200;
-//处理失败
-const API_ERROR = 400;
 // 数据验证失败
-const API_VAILD_EXCEPTION = 403;
+const API_VAILD_EXCEPTION = 400;
 // 登录超时
 const API_TIMEOUT = 401;
 // 服务器处理异常 / 客户端自定义处理
 const API_EXCEPTION = 402;
+
+// 访问拒绝
+const API_ACCESS_DENIED = 403;
 // 其他错误
 const API_OTHER_ERROR = 0;
 
@@ -19,8 +20,9 @@ CONST HOUR_SECOND = 3600;
 CONST DAY_SECOND = HOUR_SECOND * 24;
 // 用户登陆超时时间 秒  1天 = 86400秒
 const TOKEN_AUTH_TIME = DAY_SECOND * 3;
-// 刷新toke的有效为 10 天
+// refresh_token 默认为10天
 const REFRESH_TOKEN_TIMEOUT = DAY_SECOND * 10;
+
 // 用户验证缓存时间 秒
 const REFRESH_TIME = 60;
 // 手机验证码绑在时间
