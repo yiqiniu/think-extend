ad2a22825ca7590c81db24842267f5674a302f5d
gjianbo <gjianbo@qq.com>
2019-12-13 17:17:41 +0800

    独立6.0的方法

--- src/Service.php
+++ src/Service.php
@@ -5,12 +5,6 @@ namespace yiqiniu;
 
 
 use think\Route;
-use yiqiniu\console\command\MakeFacade;
-use yiqiniu\console\command\ModelAll;
-use yiqiniu\console\command\Socket;
-use yiqiniu\console\command\UuidKey;
-use yiqiniu\console\command\ValidateAll;
-use yiqiniu\console\command\ValidateTable;
 use yiqiniu\filesystem\Oss;
 use yiqiniu\filesystem\Qiniu;
 
@@ -20,17 +14,5 @@ class Service extends \think\Service
     {
         $this->app->bind('qiniu', Qiniu::class);
         $this->app->bind('oss', Oss::class);
-    }
-
-    public function boot(Route $route)
-    {
-        $this->commands([
-            Socket::class,
-            ModelAll::class,
-            ValidateAll::class,
-            ValidateTable::class,
-            MakeFacade::class,
-            UuidKey::class
-        ]);
     }
 }
